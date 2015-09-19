5630982e4c6ff9dad47a67ba04bd0fe33d350954
Michael Ligus <maveilthain@gmail.com>
2015-09-19 21:21:59 +0200

    #4 - another idea for html tags attributes

---
+++ tags/html/Attribute.php
@@ -0,0 +1,258 @@
+<?php
+
+
+namespace liphte\tags\html;
+
+/**
+ * Class Tag
+ * @package liphte\html\tags
+ *
+ * @method static Attribute abbr  ( array $content = array () )
+ * @method static Attribute accept ( array $content = array () )
+ * @method static Attribute acceptCharset ( array $content = array () )
+ * @method static Attribute accesskey ( array $content = array () )
+ * @method static Attribute action ( array $content = array () )
+ * @method static Attribute allowfullscreen ( array $content = array () )
+ * @method static Attribute alt ( array $content = array () )
+ * @method static Attribute async ( array $content = array () )
+ * @method static Attribute autocomplete ( array $content = array () )
+ * @method static Attribute autofocus ( array $content = array () )
+ * @method static Attribute autoplay ( array $content = array () )
+ * @method static Attribute challenge ( array $content = array () )
+ * @method static Attribute charset ( array $content = array () )
+ * @method static Attribute checked ( array $content = array () )
+ * @method static Attribute cite ( array $content = array () )
+ * @method static Attribute c1ass ( array $content = array () )
+ * @method static Attribute cols ( array $content = array () )
+ * @method static Attribute colspan ( array $content = array () )
+ * @method static Attribute command ( array $content = array () )
+ * @method static Attribute content ( array $content = array () )
+ * @method static Attribute contenteditable ( array $content = array () )
+ * @method static Attribute contextmenu ( array $content = array () )
+ * @method static Attribute controls ( array $content = array () )
+ * @method static Attribute coords ( array $content = array () )
+ * @method static Attribute crossorigin ( array $content = array () )
+ * @method static Attribute data ( array $content = array () )
+ * @method static Attribute datetime ( array $content = array () )\
+ * @method static Attribute default ( array $content = array () )
+ * @method static Attribute defer ( array $content = array () )
+ * @method static Attribute dir ( array $content = array () )
+ * @method static Attribute dirname ( array $content = array () )
+ * @method static Attribute disabled ( array $content = array () )
+ * @method static Attribute download ( array $content = array () )
+ * @method static Attribute draggable ( array $content = array () )
+ * @method static Attribute dropzone ( array $content = array () )
+ * @method static Attribute enctype ( array $content = array () )
+ * @method static Attribute for ( array $content = array () )
+ * @method static Attribute form ( array $content = array () )
+ * @method static Attribute formaction ( array $content = array () )
+ * @method static Attribute formenctype ( array $content = array () )
+ * @method static Attribute formmethod ( array $content = array () )
+ * @method static Attribute formnovalidate ( array $content = array () )
+ * @method static Attribute formtarget ( array $content = array () )
+ * @method static Attribute headers ( array $content = array () )
+ * @method static Attribute height ( array $content = array () )
+ * @method static Attribute hidden ( array $content = array () )
+ * @method static Attribute high ( array $content = array () )
+ * @method static Attribute href ( array $content = array () )
+ * @method static Attribute hreflang ( array $content = array () )
+ * @method static Attribute http-equiv ( array $content = array () )
+ * @method static Attribute icon ( array $content = array () )
+ * @method static Attribute id ( array $content = array () )
+ * @method static Attribute inputmode ( array $content = array () )
+ * @method static Attribute ismap ( array $content = array () )
+ * @method static Attribute itemid ( array $content = array () )
+ * @method static Attribute itemprop ( array $content = array () )
+ * @method static Attribute itemref ( array $content = array () )
+ * @method static Attribute itemscope ( array $content = array () )
+ * @method static Attribute itemtype ( array $content = array () )
+ * @method static Attribute keytype ( array $content = array () )
+ * @method static Attribute kind ( array $content = array () )
+ * @method static Attribute label ( array $content = array () )
+ * @method static Attribute lang ( array $content = array () )
+ * @method static Attribute list ( array $content = array () )
+ * @method static Attribute loop ( array $content = array () )
+ * @method static Attribute low ( array $content = array () )
+ * @method static Attribute manifest ( array $content = array () )
+ * @method static Attribute max ( array $content = array () )
+ * @method static Attribute maxlength ( array $content = array () )
+ * @method static Attribute media ( array $content = array () )
+ * @method static Attribute mediagroup ( array $content = array () )
+ * @method static Attribute menu ( array $content = array () )
+ * @method static Attribute method ( array $content = array () )
+ * @method static Attribute min ( array $content = array () )
+ * @method static Attribute minlength ( array $content = array () )
+ * @method static Attribute multiple ( array $content = array () )
+ * @method static Attribute muted ( array $content = array () )
+ * @method static Attribute name ( array $content = array () )
+ * @method static Attribute nonce ( array $content = array () )
+ * @method static Attribute novalidate ( array $content = array () )
+ * @method static Attribute open ( array $content = array () )
+ * @method static Attribute optimum ( array $content = array () )
+ * @method static Attribute pattern ( array $content = array () )
+ * @method static Attribute placeholder ( array $content = array () )
+ * @method static Attribute poster ( array $content = array () )
+ * @method static Attribute preload ( array $content = array () )
+ * @method static Attribute radiogroup ( array $content = array () )
+ * @method static Attribute readonly ( array $content = array () )
+ * @method static Attribute rel ( array $content = array () )
+ * @method static Attribute required ( array $content = array () )
+ * @method static Attribute reversed ( array $content = array () )
+ * @method static Attribute rows ( array $content = array () )
+ * @method static Attribute rowspan ( array $content = array () )
+ * @method static Attribute sandbox ( array $content = array () )
+ * @method static Attribute spellcheck ( array $content = array () )
+ * @method static Attribute scope ( array $content = array () )
+ * @method static Attribute scoped ( array $content = array () )
+ * @method static Attribute seamless ( array $content = array () )
+ * @method static Attribute selected ( array $content = array () )
+ * @method static Attribute shape ( array $content = array () )
+ * @method static Attribute size ( array $content = array () )
+ * @method static Attribute sizes ( array $content = array () )
+ * @method static Attribute sortable ( array $content = array () )
+ * @method static Attribute sorted ( array $content = array () )
+ * @method static Attribute span ( array $content = array () )
+ * @method static Attribute src ( array $content = array () )
+ * @method static Attribute srcdoc ( array $content = array () )
+ * @method static Attribute srclang ( array $content = array () )
+ * @method static Attribute srcset ( array $content = array () )
+ * @method static Attribute start ( array $content = array () )
+ * @method static Attribute step ( array $content = array () )
+ * @method static Attribute style ( array $content = array () )
+ * @method static Attribute tabindex ( array $content = array () )
+ * @method static Attribute target ( array $content = array () )
+ * @method static Attribute title ( array $content = array () )
+ * @method static Attribute translate ( array $content = array () )
+ * @method static Attribute type ( array $content = array () )
+ * @method static Attribute typemustmatch ( array $content = array () )
+ * @method static Attribute usemap ( array $content = array () )
+ * @method static Attribute value ( array $content = array () )
+ * @method static Attribute width ( array $content = array () )
+ * @method static Attribute wrap ( array $content = array () )
+ * @method static Attribute onabort ( array $content = array () )
+ * @method static Attribute onautocomplete ( array $content = array () )
+ * @method static Attribute onautocompleteerror ( array $content = array () )
+ * @method static Attribute onafterprint ( array $content = array () )
+ * @method static Attribute onbeforeprint ( array $content = array () )
+ * @method static Attribute onbeforeunload ( array $content = array () )
+ * @method static Attribute onblur ( array $content = array () )
+ * @method static Attribute oncancel ( array $content = array () )
+ * @method static Attribute oncanplay ( array $content = array () )
+ * @method static Attribute oncanplaythrough ( array $content = array () )
+ * @method static Attribute onchange ( array $content = array () )
+ * @method static Attribute onclick ( array $content = array () )
+ * @method static Attribute onclose ( array $content = array () )
+ * @method static Attribute oncontextmenu ( array $content = array () )
+ * @method static Attribute oncuechange ( array $content = array () )
+ * @method static Attribute ondblclick ( array $content = array () )
+ * @method static Attribute ondrag ( array $content = array () )
+ * @method static Attribute ondragend ( array $content = array () )
+ * @method static Attribute ondragenter ( array $content = array () )
+ * @method static Attribute ondragexit ( array $content = array () )
+ * @method static Attribute ondragleave ( array $content = array () )
+ * @method static Attribute ondragover ( array $content = array () )
+ * @method static Attribute ondragstart ( array $content = array () )
+ * @method static Attribute ondrop ( array $content = array () )
+ * @method static Attribute ondurationchange ( array $content = array () )
+ * @method static Attribute onemptied ( array $content = array () )
+ * @method static Attribute onended ( array $content = array () )
+ * @method static Attribute onerror ( array $content = array () )
+ * @method static Attribute onfocus ( array $content = array () )
+ * @method static Attribute onhashchange ( array $content = array () )
+ * @method static Attribute oninput ( array $content = array () )
+ * @method static Attribute oninvalid ( array $content = array () )
+ * @method static Attribute onkeydown ( array $content = array () )
+ * @method static Attribute onkeypress ( array $content = array () )
+ * @method static Attribute onkeyup ( array $content = array () )
+ * @method static Attribute onlanguagechange ( array $content = array () )
+ * @method static Attribute onload ( array $content = array () )
+ * @method static Attribute onloadeddata ( array $content = array () )
+ * @method static Attribute onloadedmetadata ( array $content = array () )
+ * @method static Attribute onloadstart ( array $content = array () )
+ * @method static Attribute onmessage ( array $content = array () )
+ * @method static Attribute onmousedown ( array $content = array () )
+ * @method static Attribute onmouseenter ( array $content = array () )
+ * @method static Attribute onmouseleave ( array $content = array () )
+ * @method static Attribute onmousemove ( array $content = array () )
+ * @method static Attribute onmouseout ( array $content = array () )
+ * @method static Attribute onmouseover ( array $content = array () )
+ * @method static Attribute onmouseup ( array $content = array () )
+ * @method static Attribute onwheel ( array $content = array () )
+ * @method static Attribute onoffline ( array $content = array () )
+ * @method static Attribute ononline ( array $content = array () )
+ * @method static Attribute onpagehide ( array $content = array () )
+ * @method static Attribute onpageshow ( array $content = array () )
+ * @method static Attribute onpause ( array $content = array () )
+ * @method static Attribute onplay ( array $content = array () )
+ * @method static Attribute onplaying ( array $content = array () )
+ * @method static Attribute onpopstate ( array $content = array () )
+ * @method static Attribute onprogress ( array $content = array () )
+ * @method static Attribute onratechange ( array $content = array () )
+ * @method static Attribute onreset ( array $content = array () )
+ * @method static Attribute onresize ( array $content = array () )
+ * @method static Attribute onscroll ( array $content = array () )
+ * @method static Attribute onseeked ( array $content = array () )
+ * @method static Attribute onseeking ( array $content = array () )
+ * @method static Attribute onselect ( array $content = array () )
+ * @method static Attribute onshow ( array $content = array () )
+ * @method static Attribute onsort ( array $content = array () )
+ * @method static Attribute onstalled ( array $content = array () )
+ * @method static Attribute onstorage ( array $content = array () )
+ * @method static Attribute onsubmit ( array $content = array () )
+ * @method static Attribute onsuspend ( array $content = array () )
+ * @method static Attribute ontimeupdate ( array $content = array () )
+ * @method static Attribute ontoggle ( array $content = array () )
+ * @method static Attribute onunload ( array $content = array () )
+ * @method static Attribute onvolumechange ( array $content = array () )
+ * @method static Attribute onwaiting ( array $content = array () )
+ *
+ *
+ */
+
+class Attribute
+{
+
+    private $name;
+    private $arguments;
+
+    private function __construct()
+    {
+
+        $args = func_get_args();
+        $numberOfArgumetns = func_num_args();
+
+        if($numberOfArgumetns > 0) {
+
+            $this->name = $args[0];
+            $this->arguments = $args[1];
+
+        }
+
+    }
+
+
+    public static function __callStatic($name, $arguments)
+    {
+
+        if ($name === 'c1ass') {
+            $name = 'class';
+        } else if ($name === 'acceptCharset') {
+            $name = 'accept-charset';
+        }
+
+        return new Attribute($name, $arguments);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getContent()
+    {
+
+        return implode(' ', $this->arguments);
+    }
+
+}
+
